d7915dcd2908f25d2479636a08bbf279583a2dd3
Shoukath Khan <sfazlullakhan@expedia.com>
2016-05-11 17:45:50 -0500

    A working version of todo app

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,50 +1,78 @@
-var TaskModel = Backbone.Model.extend({
-	defaults: {
-		name: 'Test',
-		done: false
-	}
+var Task = Backbone.Model.extend({
+  defaults: {
+    done: false
+  }
 });
 
-var TaskCollection = Backbone.Collection.extend();
+var TaskList = Backbone.Collection.extend({
+  model: Task
+});
 
 var TaskView = Backbone.View.extend({
-	tagName: 'li',
-	template: _.template($('#taskItemTemplate').html()),
-	initialize: function() {
+  tagName: 'li',
+  events: {
+    'click input': 'statusUpdated'
+  },
+  template: Handlebars.compile($('#taskItem').html()),
+  render: function() {
+    var markup = this.template(this.model.toJSON());
+    this.$el.html(markup);
 
-	},
-	render: function() {
-		var markup = this.template(this.model.toJSON());;
-		this.$el.html(markup);
-		
-		return this;
-	}
+    return this;
+  },
+  statusUpdated: function() {
+    var checkboxStatus = this.$el.find('input').is(':checked');
+    this.model.set('done', checkboxStatus);
+
+    if (checkboxStatus) {
+      this.$el.find('.taskName').addClass('strike');
+    } else {
+      this.$el.find('.taskName').removeClass('strike');
+    }
+
+  }
+
 });
 
 var TaskListView = Backbone.View.extend({
-	el: '#taskList',
-	initialize: function() {
-		this.listenTo(this.collection, 'change', this.render);
-	},
-	render: function() {
-		this.collection.each(this.addTask, this);
-	},
-	addTask: function(item) {
-		var taskItem = new TaskView({
-			model: item
-		});
-		this.$el.append(taskItem.render().el);
-	}
+  el: '#todoList',
+  initialize: function() {
+    this.listenTo(this.collection, 'add', this.render);
+  },
+  render: function() {
+    this.$el.empty();
+    this.collection.each(this.addTask, this);
+  },
+  addTask: function(task) {
+    var taskView = new TaskView({
+      model: task
+    });
+    var renderedTaskItem = taskView.render().$el;
+    this.$el.append(renderedTaskItem);
+  }
 });
 
-var taskModelInstance = new TaskModel();
 
-var taskModelInstance1 = new TaskModel();
+//Implementing the Backbone objects
+var taskList = new TaskList();
 
-var taskCollectionInstance = new TaskCollection([taskModelInstance, taskModelInstance1]);
-
-var taskViewInstance = new TaskListView({
-	collection: taskCollectionInstance
+var taskListView = new TaskListView({
+  collection: taskList
 });
 
-taskViewInstance.render();
+
+// Events
+$('#todoInput').keypress(function(event) {
+  var taskName = $(this).val();
+  if( event.keyCode === 13 && taskName.length) {
+    var task = new Task({name: taskName});
+    taskList.add(task);
+
+    $(this).val('');
+  }
+});
+
+// $('#todoList').on('click', 'input', function(event) {
+//   console.log('Clicked');
+//   //
+// });
